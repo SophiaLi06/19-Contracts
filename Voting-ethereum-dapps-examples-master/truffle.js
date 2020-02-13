873909ccb7d50bbb20c78b3033f2c100b1aeefb4
Minghao Li <ml922@cornell.edu>
2020-02-12 22:30:16 -0500

    Initialize

---
+++ Voting-ethereum-dapps-examples-master/truffle.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    rpc: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
+    }
+  },
+	compilers:{
+  solc:{
+    version:"0.4.4"
+  }
+}
+}
